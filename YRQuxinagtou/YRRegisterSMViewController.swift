534b6c219c270fc276967b0f97e9b9453c59b95c
YongRen <zhanxin13@gmail.com>
2016-10-10 18:27:30 +0800

    register added

---
+++ YRQuxinagtou/YRRegisterSMViewController.swift
@@ -0,0 +1,35 @@
+//
+//  YRRegisterSMViewController.swift
+//  YRQuxinagtou
+//
+//  Created by YongRen on 2016/10/10.
+//  Copyright © 2016年 YongRen. All rights reserved.
+//
+
+import UIKit
+
+class YRRegisterSMViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
